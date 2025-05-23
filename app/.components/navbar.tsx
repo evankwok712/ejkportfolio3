b9420ca0181f8a7d16e2cfa1b526008196a061fb
Evan Kwok <evan.jesse.kwok@gmail.com>
2025-05-22 22:51:04 -0400

    worked on display page template

--- app/.components/navbar.tsx
+++ app/.components/navbar.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Link from "next/link";
 const Navbar: React.FC = () => {
     return (
-        <nav className="bg-white border-gray-200 fixed top-0 left-0 z-50 w-full text-gray-400">
+        <nav className="border-gray-200 fixed top-0 left-0 z-50 w-full text-gray-400">
             <div className="w-full flex flex-wrap items-center justify-between p-4">
-                <Link href="/pageone" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <Link href="/home" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <p>ejk</p>
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
                 </Link>

c71ca9df42285e09e127d6f278f0bf1c3012f60a
Evan Kwok <evan.jesse.kwok@gmail.com>
2025-06-17 15:24:41 -0400

    edits

--- app/blob-complex/page.tsx
+++ app/blob-complex/page.tsx
@@ -3,6 +3,12 @@ import Image from "next/image";
 import Navbar from "@/app/.components/navbar";
 import Footer from "@/app/.components/footer";
 import blobStruc01 from "@/app/.images/blob-01.jpg";
+import blobStruc02 from "@/app/.images/blob-02.jpg";
+import blobStruc03 from "@/app/.images/blob-03.jpg";
+import blobStruc04 from "@/app/.images/blob-04.jpg";
+import blobStruc05 from "@/app/.images/blob-05.jpg";
+import blobDrawing from "@/app/.images/parametric-structure-04.png";
+import blobGrasshopper from "@/app/.images/parametric-structure-grasshopper-script.png";
 
 const BlobComplex: React.FC = () => {
     return (
@@ -15,7 +21,7 @@ const BlobComplex: React.FC = () => {
             </div>
             <div className="pb-30 px-40 py-10">
                 <div>
-                    <p>FIKA Cafe</p>
+                    <p>Blob Structure</p>
                     <p>Drawing Exercise</p>
                     <p>2023</p>
                     <br />
